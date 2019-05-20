50e941aaff18765a8af4ae8f3c94c266b7bd40d4
Will Clark <wclark@patientkeeper.com>
2019-05-20 16:41:21 -0400

    init spring boot skeleton generated via https://start.spring.io/

---
+++ src/main/java/org/willclark/oauthsforce/OauthSforceApplication.java
@@ -0,0 +1,13 @@
+package org.willclark.oauthsforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthSforceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthSforceApplication.class, args);
+	}
+
+}
